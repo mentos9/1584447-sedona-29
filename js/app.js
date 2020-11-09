ff7d3392b155349ee25d91b7ba49076af5648eaf
mentos9 <71405154+mentos9@users.noreply.github.com>
2020-11-09 22:47:30 +0300

    ё121

--- js/app.js
+++ js/app.js
@@ -25,4 +25,19 @@ let search = document.getElementById('search_hostel_calendar');
 searchHostel.addEventListener("click", function () {
  search.classList.toggle("visibility");
  console.log('1');
+});
+
+
+
+
+const form = document.querySelector("form");
+
+const dateIn = form.querySelector("[name=date_in]");
+const dateOut = form.querySelector("[name=date_out]");
+const adultYears = form.querySelector("[name=adult_years]");
+const childYears = form.querySelector("[name=child_years]");
+
+form.addEventListener("submit", function (evt) {
+evt.preventDefault();
+console.log(adultYears.value);
 })
